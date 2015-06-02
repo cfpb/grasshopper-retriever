09e85c801d15e29ec9911380fc7558734b23bc24
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-06-02 11:43:18 -0400

    Modularize and fork for local vs S3

--- index.js
+++ index.js
@@ -4,46 +4,77 @@ var spawn = require('child_process').spawn;
 var hyperquest = require('hyperquest');
 var unzip = require('unzip');
 var csvToVrt = require('csv-to-vrt');
+var uploadStream = require('./lib/uploadStream');
 
 var zipReg = /.zip$/i;
 var csvReg = /(?:txt|csv)$/i;
 var restrictedReg = /\.\.|\//g;
 
-//provide data file and output path
-var data = JSON.parse(fs.readFileSync('maine.json'));
+var program;
 
-data.forEach(function(record){
-  //Don't allow to traverse to other folders via data.json
-  var name = record.name = record.name.replace(restrictedReg,'');
-  var request = hyperquest(record.url);
-  if(zipReg.test(record.url)){
-    request.pipe(unzip.Extract({path: name}))
-      .on('close', function(){
-        var unzipped = path.join(name, record.file)
-        if(csvReg.test(record.file)){
-          csvToVrt(unzipped, record.sourceSrs, function(vrt){
-            handleStream(spawnOgr(vrt), record);
-          });
-        }else{
-          handleStream(spawnOgr(unzipped), record);
-        }
-      })
-  }
-});
-
-
-function spawnOgr(file, stream){
-  var child; 
-  if(stream){
-    child = spawn('ogr2ogr', ['-f', 'CSV', '-t_srs', 'WGS84', -'lco', 'GEOMETRY=AS_XY', '/vsistdout/', '/vsistdin/'])
-    stream.pipe(child.stdin);
-  }else{
-    child = spawn('ogr2ogr', ['-f', 'CSV', '-t_srs', 'WGS84', '-lco', 'GEOMETRY=AS_XY', '/vsistdout/', file])
-  }
-  return child.stdout;
+if(require.main === module){
+  program = require('commander');
+  program
+    .version('0.0.1')
+    .option('-b, --bucket <bucket>', 'An S3 bucket where the data should be loaded.')
+    .option('-p, --profile <profile>', 'The aws profile in ~/.aws/credentials. Will also respect environmental variables.', 'default')
+    .option('-d, --directory <directory>', 'A directory where the data should be loaded, either relative to the current folder or the passed S3 bucket.', '.')
+    .option('-f --file <file>', 'The json data file that contains the collected data endpoints. Defaults to data.json.', 'data.json')
+    .parse(process.argv)
+  run(program);
 }
 
-function handleStream(stream, record){
-  var endfile = path.join('output', record.name + '.csv');
-  stream.pipe(fs.createWriteStream(endfile));
+function run(program, callback){
+  var data = JSON.parse(fs.readFileSync(program.file));
+
+  if(program.bucket) uploadStream.init(program.bucket, program.profile) 
+
+  data.forEach(function(record){
+    //Don't allow to traverse to other folders via data.json
+    var name = record.name = record.name.replace(restrictedReg,'');
+    var request = hyperquest(record.url);
+
+    if(zipReg.test(record.url)){
+      request.pipe(unzip.Extract({path: name}))
+        .on('close', function(){
+          var unzipped = path.join(name, record.file)
+          if(csvReg.test(record.file)){
+            csvToVrt(unzipped, record.sourceSrs, function(vrt){
+              handleStream(spawnOgr(vrt), record);
+            });
+          }else{
+            handleStream(spawnOgr(unzipped), record);
+          }
+        });
+    }else{
+      handleStream(spawnOgr(null, request), record)
+    }
+  });
+
+
+  function spawnOgr(file, stream){
+    var child; 
+    if(stream){
+      child = spawn('ogr2ogr', ['-f', 'CSV', '-t_srs', 'WGS84', -'lco', 'GEOMETRY=AS_XY', '/vsistdout/', '/vsistdin/'])
+      stream.pipe(child.stdin);
+    }else{
+      child = spawn('ogr2ogr', ['-f', 'CSV', '-t_srs', 'WGS84', '-lco', 'GEOMETRY=AS_XY', '/vsistdout/', file])
+    }
+    return child.stdout;
+  }
+
+
+  function handleStream(stream, record, cb){
+    if(!cb) cb = function(){};
+
+    var endfile = path.join(program.directory, record.name + '.csv');
+
+    if(program.bucket){
+      return uploadStream.stream(stream, endfile, cb);
+    }
+
+    stream.pipe(fs.createWriteStream(endfile))
+      .on('finish', cb);
+  }
+
 }
