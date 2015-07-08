fd8d989bd926af5135d83ffa5d7eda7d45800f52
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-07-08 14:52:17 -0400

    Remove default match and fix retriever.js output

--- retriever.js
+++ retriever.js
@@ -11,15 +11,15 @@ program
   .option('-p, --profile <profile>', 'The aws profile in ~/.aws/credentials. Will also respect environmental variables.', 'default')
   .option('-d, --directory <directory>', 'A directory where the data should be loaded, either relative to the current folder or the passed S3 bucket.', '.')
   .option('-f --file <file>', 'The json data file that contains the collected data endpoints.')
-  .option('-m --match <match>', 'A string or regular expression that the names from the <file> must contain or match', '')
+  .option('-m --match <match>', 'A string or regular expression that the names from the <file> must contain or match')
   .option('-q --quiet', 'Suppress logging.', false)
   .parse(process.argv);
 
 retriever(program, function(errs, retrieved){
   console.log('Fetched %d source%s and placed %s in %s%s',
-    retrieved,
-    retrieved === 1 ? '' : 's',
-    retrieved === 1 ? 'it' : 'them',
+    retrieved.length,
+    retrieved.length === 1 ? '' : 's',
+    retrieved.length === 1 ? 'it' : 'them',
     program.bucket ? program.bucket + '/' : '',
     program.directory
   );
