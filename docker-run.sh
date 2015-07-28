315ebe645e13bf0724d77c889db0dd25abab3638
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-07-28 15:39:43 -0400

    update scripts to no longer depend on environment variables

--- docker-run.sh
+++ docker-run.sh
@@ -4,7 +4,6 @@ image="$1"
 shift
 
 docker run --rm\
-  -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"\
-  -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"\
+  -v "`echo $HOME`/.aws:/home/notroot/.aws"\
   "$image"\
   "$@"
